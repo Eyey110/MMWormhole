8230e4a739e2bd709fa4c37471f4b1da1bedc9f4
郑了 <zhengle@zhengledeMacBook-Pro.local>
2025-06-24 16:59:22 +0800

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+import PackageDescription
+
+let package = Package(
+    name: "MMWormhole",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "MMWormhole",
+            targets: ["MMWormhole"]),
+    ],
+    targets: [
+        .target(
+            name: "MMWormhole",
+            path: "Source",
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath(".")
+            ]
+    ])
